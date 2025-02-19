cf3ffa6d00e29a5be028fdebc026fd78c8cf39d2
Domnic <apple@domnic.local>
2025-02-19 19:06:01 +0530

    chore: add env

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosUnauthInstance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: process.env.BASE_URL, 
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
